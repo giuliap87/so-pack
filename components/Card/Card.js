f55b57ac28bd37c3020d5a1ab85edd6897ff50de
giuliaeremite <prsgiulia@gmail.com>
2021-01-31 19:29:29 +0100

    placed image in card as background

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -2,10 +2,14 @@ import styles from "./Card.module.css";
 import Button from "@material-ui/core/Button";
 import Link from "next/link";
 
+//background: linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url("https://i.imgur.com/xnh5x47.jpg");
+
 function MyCard({ id, title, description, path }) {
   return (
-    <div className={styles.container}>
-      <img alt="first" className={styles.image} src={`/images/${id}.png`} />
+    <div
+      className={styles.container}
+      style={{ backgroundImage: `linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url(/images/${id}.png)`}}
+    >
       <div className={styles.body}>
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.text}>{description}</p>
